d9cdc6fc19338990bdb82ac52562d09262d2ac9b
nika <omiadzenika6@gmail.com>
2025-04-16 16:09:34 +0400

    Add project files.

---
+++ dockertest/Program.cs
@@ -0,0 +1,42 @@
+
+namespace dockertest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var port = Environment.GetEnvironmentVariable("port") ?? "8080";
+            builder.WebHost.UseUrls($"http://*:{port}");
+
+            builder.Services.AddHealthChecks();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHealthChecks("/healt");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
