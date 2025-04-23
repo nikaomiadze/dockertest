1adc286e95e49a1e4cd4fcc6ab72ea3284792df2
nika <119878142+nikaomiadze@users.noreply.github.com>
2025-04-23 23:20:56 +0400

    modify sensordata model

--- dockertest/models/SensorData.cs
+++ dockertest/models/SensorData.cs
@@ -1,5 +1,6 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace dockertest.models
@@ -11,18 +12,23 @@ namespace dockertest.models
         public string? Id { get; set; }
 
         [Required]
-        [StringLength(6, MinimumLength = 6)]
-        public string? DeviceId { get; set; }
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "DeviceId must be exactly 6 characters.")]
+        [BsonElement("deviceId")]
+        public string DeviceId { get; set; } = string.Empty;
 
+        [BsonElement("timestamp")]
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 
-        [Range(-40, 80)]
+        [Range(-40, 80, ErrorMessage = "Temperature must be between -40 and 80 Celsius.")]
+        [BsonElement("temperature")]
         public double Temperature { get; set; }
 
-        [Range(300, 5000)]
+        [Range(300, 5000, ErrorMessage = "CO2 level must be between 300 and 5000 ppm.")]
+        [BsonElement("co2")]
         public int CO2 { get; set; }
 
-        [Range(0,100)]
+        [Range(0, 100, ErrorMessage = "Humidity must be between 0% and 100%.")]
+        [BsonElement("humidity")]
         public int Humidity { get; set; }
     }
 }
